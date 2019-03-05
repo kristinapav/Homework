ad34e55e64970230041c1aa6554375bcfdddc9f0
kristinaov <kpavlovska666@gmail.com>
2019-03-05 13:03:34 +0100

    HomeworkPart3

---
+++ HomeworkPart3/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkPart3
+{
+    class Program
+    {
+        private static string d;
+
+        static void Main(string[] args)
+        {
+            ////Task 1.
+
+            //Console.Write("Please write number :   "); 
+            //int n = Int32.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= n; i++)
+            //{ 
+            //   if (i % 3 == 0)
+            //    {
+            //        Console.WriteLine("{0}", i);
+            //    }
+            //}
+            //Console.ReadKey();
+
+
+            //Task 2.
+
+            //Console.Write("Please write number :   ");
+            //int m = Int32.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= m; i++)
+            //{
+            //    if (i % 3 == 0 && i % 2 == 0)
+            //    {
+            //        Console.WriteLine("{0}", i);
+            //    }
+            //}
+            //Console.ReadKey();
+
+
+            //Task 3.
+
+            //int x, y, result;
+
+            //Console.Write("Enter first number :   ");
+            //x = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Enter second number :  ");
+            //y = Convert.ToInt32(Console.ReadLine());
+
+            //result = x * y;
+            //Console.Write("The product of the two numbers is : " + result);
+
+            //Console.ReadLine();
+
+            //Task 5.
+
+            //int i, num, sum = 0;
+
+            //Console.Write("Enter any number : " );
+            //num = Convert.ToInt32(Console.ReadLine());
+
+            //for (i = 1; i <= num; i += 2)
+            //{
+            //    sum += i;
+            //}
+            //Console.WriteLine("Sum of all odd numbers between 1 and " + num + " = " + sum);
+
+
+            //Task 6.
+
+            //int i, num, sum = 0;
+            //Console.Write("Enter any number : ");
+            //num = Convert.ToInt32(Console.ReadLine());
+
+            //for (i = 1; i <= num; i ++ )
+            //{
+            //    if (i % 5 == 0)
+            //    {
+            //        sum += i;
+            //    }
+            //}
+
+            //Console.WriteLine("Sum of all numbers divided by 5   = " + sum);
+
+            //Task 7.
+
+            //string[] array = new string[4];
+
+            //array[0] = "a";
+            //array[1] = "b";
+            //array[2] = "c";
+            //array[3] = "d";
+
+            //foreach (string i in array)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Array.Reverse(array);
+            //Console.WriteLine("Reversed array : " );
+
+            //foreach (string j in array)
+            //{
+            //    Console.WriteLine(j);
+            //}
+            //Console.ReadLine();
+
+            //Task 8
+
+            //int[] arr = new int[] { 999, 87, 65, 5 };
+            //int[] brr = new int[] { 99, 87, 56, 45 };
+
+            ////Returns only true or false
+
+            //Console.WriteLine(arr.SequenceEqual(brr));
+
+
+            //Task 9
+
+            //Task 10
+
+            //Task 11
+
+            //Task 12
+
+
+        }
+    }
+}
